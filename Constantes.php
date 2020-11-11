9f3b393ac3d7c34dc3a76dc8f2bf79bc44d2d00a
epitheca <mathieu.moncomble@epitheca.fr>
2020-11-11 18:40:32 +0100

    Ajout des constantes à compléter

---
+++ Constantes.php
@@ -0,0 +1,37 @@
+<!--
+Copyright Mathieu MONCOMBLE (contact@epitheca.fr) 2009-2020
+
+This file is part of epitheca.
+
+    epitheca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    epitheca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with epitheca.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
+<?php
+// Fichier contenant les definitions de constantes
+define ("CHEMIN","");
+define ("CHEMIN_URL","");
+define ("NOM","");
+define ("PASSE", "");
+define ("SERVEUR", "");
+define ("BASE", "");
+
+//variables pour sites
+$CLE_reCAPTCHA_site="";
+$CLE_reCAPTCHA_secrete="";
+$mail_administrateur="";
+
+// Format de représentation d'une ligne d'une table
+define ("FORMAT_OBJET", "objet");
+define ("FORMAT_TABLEAU", "tableau");
+
+?>
---
+++ Util.php
@@ -0,0 +1,44 @@
+<?php
+// Inclusion des utilitaires du site NV
+
+// Les constantes -> chemin en dur
+require_once ("");
+
+
+// Les fonctions générales
+require_once (CHEMIN."HTML.php");  
+
+// Modules et classes
+require_once (CHEMIN."Tableau.class.php");
+require_once (CHEMIN."BDMySQL.class.php");
+
+// Fonctions du site
+require_once (CHEMIN."Design.php");
+
+// Fonctions diverses et variées
+require_once (CHEMIN."NormalisationHTTP.php"); 
+require_once (CHEMIN."fonctions.php"); 
+require_once (CHEMIN."Session.php");
+require_once (CHEMIN."GestionErreurs.php");
+require_once (CHEMIN."GestionExceptions.php");
+
+// Si on est en échappement automatique, on rectifie...
+// NB: on peut optimiser en utilisant des références si nécessaire.
+
+if (get_magic_quotes_gpc())
+{
+  $_POST = NormalisationHTTP($_POST);
+  $_GET = NormalisationHTTP($_GET);
+  $_REQUEST = NormalisationHTTP($_REQUEST);
+  $_COOKIE = NormalisationHTTP($_COOKIE);
+}
+
+// Réglage du niveau d'erreur
+error_reporting(E_ALL | E_STRICT);
+
+// Gestionnaire d'erreurs personnalisé. Voir GestionErreurs.php.
+set_error_handler("GestionErreurs");
+
+// Gestionnaire d'exceptions personnalisé. Voir GestionExceptions.php.
+set_exception_handler("GestionExceptions");
+?>
